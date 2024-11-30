1cd2faf9112bf9eb40debebe5d8167a00109e537
Adarsh K <adarshbhatk@gmail.com>
2024-11-30 21:49:52 +0530

    Add temperature and location components

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import './Weather.css';
 import search_icon from '../assets/search.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
+import drizzle_icon from '../assets/drizzle.png';
+import rain_icon from '../assets/rain.png';
+import snow_icon from '../assets/snow.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
 
 function Weather() {
     return (
-        <div>
-          <div className="weather">
-            <div className="search-bar">
-                <input type="text" placeholder='Search' />
-                <img src={search_icon} alt="Search icon" />
-            </div>
+        <div className="weather">
+          <div className="search-bar">
+            <input type="text" placeholder='Search' />
+            <img src={search_icon} alt="Search icon" />
           </div>
+          <img src={clear_icon} alt=" Weather icon" className='weather-icon' />
+          <p className='temperature'>22°C</p>
+          <p className='location'>Bengaluru</p>
         </div>
-      )
+      );
 }
 
 export default Weather;
